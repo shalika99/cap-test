a8e5c016fe92314f5358f0d8dd692867109f25f5
sharika <sharika@MS-no-iMac.local>
2016-12-20 14:27:53 +0900

    unicorn path

--- config/unicorn/unicorn.rb
+++ config/unicorn/unicorn.rb
@@ -1,38 +1,36 @@
-root = "deploy2/cap-test/current" # e.g. /var/apps/rails_blog/current
-working_directory root
+#root = "deploy2/cap-test/current" # e.g. /var/apps/rails_blog/current
+#working_directory root
+@app_path = 'deploy2/cap-test/current'
+working_directory @app_path + "/current"
 
-pid "#{root}/tmp/pids/unicorn.pid"
-
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
-worker_processes 8 # update this with your preference
-timeout 30
+worker_processes 2
 preload_app true
-
-listen '/tmp/unicorn.[APP_NAME].sock', backlog: 64
-
+timeout 30
+listen "/tmp/unicorn.sock", backlog: 64
+pid "#{@app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{@app_path}/current/log/unicorn.stderr.log"
+stdout_path "#{@app_path}/current/log/unicorn.stdout.log"
+# before_fork do |server, worker|
+#   ENV['BUNDLE_GEMFILE'] = File.expand_path('Gemfile', ENV['RAILS_ROOT'])
+# end
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
-
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
-end
-
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
 end
-
-# Force the bundler gemfile environment variable to
-# reference the capistrano "current" symlink
-before_exec do |_|
-  ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "/var/www/cafeapp/current/Gemfile"
+  Dotenv.overload
 end
